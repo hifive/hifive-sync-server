0be9c33850cafab5bff6b33bd3ba4a8a08ec686e
kishigam <kishigami.kazuhiro@ns-sol.co.jp>
2012-09-05 16:57:22 +0900

    SyncResourceオブジェクトごとのLockManagerインスタンス設定方法を、あらかじめMapに保持したクラスオブジェクトから生成するように変更。 （@Transactionalを有効化したことによりSyncResourceがプロキシとなり、ApplicationContext構築後にSyncResourceServiceアノテーションの情報を取得できなくなったと考えられる）

--- hifiveSyncServer/src/main/java/com/htmlhifive/sync/resource/SyncResourceManager.java
+++ hifiveSyncServer/src/main/java/com/htmlhifive/sync/resource/SyncResourceManager.java
@@ -34,103 +34,107 @@ import com.htmlhifive.sync.resource.separated.SeparatedCommonDataSyncResource;
 /**
  * アプリケーション内に存在するリソースを管理するサービスオブジェクト.<br>
  * リソースをクラス内に保持し、必要に応じて呼び出すことができます.
- *
+ * 
  * @author kishigam
  */
 @Service
 public class SyncResourceManager {
 
-    /**
-     * リソース検索対象のコンテキスト.
-     */
-    @Resource
-    private ApplicationContext context;
+	/**
+	 * リソース検索対象のコンテキスト.
+	 */
+	@Resource
+	private ApplicationContext context;
 
-    /**
-     * アプリケーション内のリソースを保持するMap
-     */
-    private Map<String, Class<? extends SyncResource<?>>> resourceMap;
+	/**
+	 * アプリケーション内のリソースを保持するMap
+	 */
+	private Map<String, Class<? extends SyncResource<?>>> resourceMap;
 
-    /**
-     * クラスパス上のリソース検索し、Mapに保持してこのクラスのインスタンスを生成します.<br>
-     * {@link SyncResource} インターフェースを実装し、{@link SyncResourceService}
-     * アノテーションを付与したクラスをリソースとします.
-     */
-    @SuppressWarnings("unchecked")
-    public SyncResourceManager() {
+	/**
+	 * リソースごとのロックマネージャを保持するMap
+	 */
+	private Map<String, Class<? extends LockManager>> lockManagerMap;
 
-        this.resourceMap = new HashMap<>();
-        try {
-            ClassPathScanningCandidateComponentProvider scanner =
-                    new ClassPathScanningCandidateComponentProvider(false);
-            scanner.addIncludeFilter(new AnnotationTypeFilter(SyncResourceService.class));
+	/**
+	 * クラスパス上のリソース検索し、Mapに保持してこのクラスのインスタンスを生成します.<br>
+	 * {@link SyncResource} インターフェースを実装し、{@link SyncResourceService} アノテーションを付与したクラスをリソースとします.
+	 */
+	@SuppressWarnings("unchecked")
+	public SyncResourceManager() {
 
-            // TODO ターゲットパッケージの指定方法検討
-            Set<BeanDefinition> beans = scanner.findCandidateComponents("");
+		this.resourceMap = new HashMap<>();
+		this.lockManagerMap = new HashMap<>();
 
-            for (BeanDefinition def : beans) {
+		try {
+			ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+			scanner.addIncludeFilter(new AnnotationTypeFilter(SyncResourceService.class));
 
-                Class<? extends SeparatedCommonDataSyncResource<?, ?>> resourceClass;
+			// TODO ターゲットパッケージの指定方法検討
+			Set<BeanDefinition> beans = scanner.findCandidateComponents("");
 
-                // null,interface,SeparatedSyncResourceのサブタイプ以外,abstractのクラスを除外
-                Class<?> found = Class.forName(def.getBeanClassName());
-                if (found == null
-                        || found.isInterface()
-                        || !SeparatedCommonDataSyncResource.class.isAssignableFrom(found)
-                        || Modifier.isAbstract(found.getModifiers())) {
-                    continue;
-                } else {
-                    resourceClass = (Class<? extends SeparatedCommonDataSyncResource<?, ?>>)found;
-                }
+			for (BeanDefinition def : beans) {
 
-                // データモデルを特定する
-                String dataModelName =
-                        resourceClass.getAnnotation(SyncResourceService.class).syncDataModel();
-                if (dataModelName == null || dataModelName.isEmpty()) {
-                    continue;
-                }
+				Class<? extends SeparatedCommonDataSyncResource<?, ?>> resourceClass;
 
-                resourceMap.put(dataModelName, resourceClass);
-            }
+				// null,interface,SeparatedSyncResourceのサブタイプ以外,abstractのクラスを除外
+				Class<?> found = Class.forName(def.getBeanClassName());
+				if (found == null || found.isInterface()
+						|| !SeparatedCommonDataSyncResource.class.isAssignableFrom(found)
+						|| Modifier.isAbstract(found.getModifiers())) {
+					continue;
+				} else {
+					resourceClass = (Class<? extends SeparatedCommonDataSyncResource<?, ?>>) found;
+				}
 
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException("An Exception thrown by SyncResourceLocator", e);
-        }
-    }
+				// リソースの情報をアノテーションから取得
+				SyncResourceService resourceDef = resourceClass.getAnnotation(SyncResourceService.class);
 
-    /**
-     * データモデルから対応するリソースを返します.
-     *
-     * @param dataModelName
-     *            データモデル名
-     * @return リソースクラス
-     */
-    public SyncResource<?> locateSyncResource(String dataModelName) {
+				// データモデルを特定する
+				String dataModelName = resourceDef.syncDataModel();
+				if (dataModelName == null || dataModelName.isEmpty()) {
+					continue;
+				}
+				resourceMap.put(dataModelName, resourceClass);
 
-        SyncResource<?> sr = context.getBean(resourceMap.get(dataModelName));
+				// ロックマネージャを特定する
+				//				resourceDef = resourceClass.getAnnotation(SyncResourceService.class);
+				lockManagerMap.put(dataModelName, resourceDef.lockManager());
+			}
 
-        // LockManagerのセット
-        SyncResourceService resourceServiceAnnotation =
-                sr.getClass().getAnnotation(SyncResourceService.class);
-        try {
-            Class<? extends LockManager> lockManagerClazz = resourceServiceAnnotation.lockManager();
-            LockManager lm = lockManagerClazz.newInstance();
-            sr.setLockManager(lm);
-        } catch (InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException("An Exception thrown by SyncResourceLocator", e);
+		}
+	}
 
-        return sr;
-    }
+	/**
+	 * データモデルから対応するリソースを返します.
+	 * 
+	 * @param dataModelName データモデル名
+	 * @return リソースクラス
+	 */
+	public SyncResource<?> locateSyncResource(String dataModelName) {
 
-    /**
-     * このクラスが管理するリソースに対応する全てのデータモデル名を返します.<br>
-     *
-     * @return データモデル名のセット
-     */
-    public Set<String> getAllDataModelNames() {
+		SyncResource<?> sr = context.getBean(resourceMap.get(dataModelName));
 
-        return resourceMap.keySet();
-    }
+		// LockManagerのセット
+		try {
+			sr.setLockManager(lockManagerMap.get(dataModelName).newInstance());
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		return sr;
+	}
+
+	/**
+	 * このクラスが管理するリソースに対応する全てのデータモデル名を返します.<br>
+	 * 
+	 * @return データモデル名のセット
+	 */
+	public Set<String> getAllDataModelNames() {
+
+		return resourceMap.keySet();
+	}
 }
