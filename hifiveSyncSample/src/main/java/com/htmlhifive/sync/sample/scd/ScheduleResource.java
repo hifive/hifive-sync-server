279e6c2f1538e4d5534223e794e968e192176d36
kishigam <kishigami.kazuhiro@ns-sol.co.jp>
2012-10-11 14:49:11 +0900

    フレームワークプロジェクトから分離し、新規プロジェクトとしてコミット.

---
+++ hifiveSyncSample/src/main/java/com/htmlhifive/sync/sample/scd/ScheduleResource.java
@@ -0,0 +1,386 @@
+/*
+ * Copyright (C) 2012 NS Solutions Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.htmlhifive.sync.sample.scd;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.htmlhifive.sync.common.ResourceItemCommonData;
+import com.htmlhifive.sync.exception.BadRequestException;
+import com.htmlhifive.sync.exception.DuplicateIdException;
+import com.htmlhifive.sync.exception.SyncException;
+import com.htmlhifive.sync.resource.AbstractSyncResource;
+import com.htmlhifive.sync.resource.ClientResolvingStrategy;
+import com.htmlhifive.sync.resource.SyncResourceService;
+import com.htmlhifive.sync.sample.person.Person;
+import com.htmlhifive.sync.sample.person.PersonRepository;
+
+/**
+ * Scheduleリソースの情報を同期リソースとして公開するためのリソースクラス.<br>
+ * 同期データを専用サービスで管理する抽象リソースクラスをこのリソース用に実装します.
+ *
+ * @author kishigam
+ */
+@SyncResourceService(resourceName = "schedule", updateStrategy = ClientResolvingStrategy.class)
+public class ScheduleResource extends AbstractSyncResource<ScheduleResourceItem> {
+
+	/**
+	 * 日付書式変換のためのフォーマット文字列(整数8桁文字列)
+	 */
+	private static final String FORMAT_STR_INT8 = "yyyyMMdd";
+
+	/**
+	 * 日付書式変換のためのフォーマット文字列(スラッシュ区切りゼロ埋めなし)
+	 */
+	private static final String FORMAT_STR_SLASH_SEPARATION = "y/M/d";
+
+	/**
+	 * エンティティの永続化を担うリポジトリ.
+	 */
+	@Resource
+	private ScheduleRepository repository;
+
+	/**
+	 * 関連するPersonエンティティを取得するためのリポジトリ.
+	 */
+	@Resource
+	private PersonRepository personRepository;
+
+	/**
+	 * 単一データreadメソッドのリソース別独自処理を行う抽象メソッド.<br>
+	 * エンティティをリポジトリから取得し、アイテムクラスのオブジェクトに設定して返します.
+	 *
+	 * @param targetItemId 対象リソースアイテムのID
+	 * @return リソースアイテム
+	 */
+	@Override
+	protected ScheduleResourceItem doGet(String targetItemId) {
+
+		Schedule bean = findSchedule(targetItemId);
+
+		ScheduleResourceItem item = entityToItem(bean);
+		return item;
+	}
+
+	/**
+	 * クエリによってリソースアイテムを取得する抽象メソッド.<br>
+	 * 指定された共通データが対応するリソースアイテムであり、かつデータ項目が指定された条件に合致するものを検索し、返します.
+	 *
+	 * @param commonDataList 共通データリスト
+	 * @param conditions 条件Map(データ項目名,データ項目の条件)
+	 * @return 条件に合致するリソースアイテム(CommonDataを値として持つMap)
+	 */
+	@Override
+	protected Map<ScheduleResourceItem, ResourceItemCommonData> doGetByQuery(
+			List<ResourceItemCommonData> commonDataList, Map<String, String[]> conditions) {
+
+		Map<ScheduleResourceItem, ResourceItemCommonData> itemMap = new HashMap<>();
+
+		// Specificationsを用いたクエリ実行
+		List<Schedule> scheduleList = repository.findAll(ScheduleSpecifications.parseConditions(commonDataList,
+				conditions));
+
+		for (Schedule schedule : scheduleList) {
+			for (ResourceItemCommonData common : commonDataList) {
+				if (common.getTargetItemId().equals(schedule.getScheduleId())) {
+					ScheduleResourceItem item = entityToItem(schedule);
+					itemMap.put(item, common);
+				}
+			}
+		}
+
+		return itemMap;
+	}
+
+	/**
+	 * エンティティの持つ情報からリソースアイテムオブジェクトに値を設定します.
+	 *
+	 * @param bean Scheduleエンティティ
+	 */
+	private ScheduleResourceItem entityToItem(Schedule bean) {
+
+		ScheduleResourceItem item = new ScheduleResourceItem(bean.getScheduleId());
+
+		List<String> userIds = new ArrayList<>();
+		for (Person user : bean.getUserBeans()) {
+			userIds.add(user.getPersonId());
+		}
+		item.setUserIds(userIds);
+
+		item.setTitle(bean.getTitle());
+		item.setCategory(bean.getCategory());
+
+		List<String> dates = new ArrayList<>();
+		for (ScheduleDate date : bean.getDateBeans()) {
+			dates.add(putDateSeparator(date.getDate()));
+		}
+		item.setDates(dates);
+
+		item.setStartTime(bean.getStartTime());
+		item.setFinishTime(bean.getFinishTime());
+		item.setDetail(bean.getDetail());
+		item.setPlace(bean.getPlace());
+		item.setCreateUserName(bean.getCreateUser().getName());
+
+		return item;
+	}
+
+	/**
+	 * 8桁数値の日付をセパレータ(スラッシュ)区切り文字列に変換します.
+	 *
+	 * @param int8Date 日付数値
+	 * @return セパレータ付き日付文字列
+	 */
+	private String putDateSeparator(int int8Date) {
+
+		DateFormat slashSeparation = new SimpleDateFormat(FORMAT_STR_SLASH_SEPARATION);
+		DateFormat int8 = new SimpleDateFormat(FORMAT_STR_INT8);
+
+		String slashSeparatedDate;
+		try {
+			slashSeparatedDate = slashSeparation.format(int8.parse(String.valueOf(int8Date)));
+		} catch (ParseException e) {
+			throw new SyncException(e);
+		}
+
+		return slashSeparatedDate;
+	}
+
+	/**
+	 * createメソッドのリソース別独自処理. <br>
+	 * エンティティを新規生成、保存し、リソースアイテムのIDを返します.
+	 *
+	 * @param newItem 生成内容を含むリソースアイテム
+	 * @return 採番されたリソースアイテムのID
+	 */
+	@Override
+	protected String doCreate(ScheduleResourceItem newItem) throws DuplicateIdException {
+
+		if (repository.exists(newItem.getScheduleId())) {
+
+			throw new DuplicateIdException(newItem.getScheduleId(), doGet(newItem.getScheduleId()));
+		}
+
+		Schedule newEntity = new Schedule(newItem.getScheduleId());
+
+		itemToEntity(newEntity, newItem);
+
+		// 作成者にログインユーザーであるPersonをセット
+		newEntity.setCreateUser(loginUser());
+
+		repository.save(newEntity);
+
+		return newEntity.getScheduleId();
+	}
+
+	/**
+	 * updateメソッドのリソース別独自処理.<br>
+	 *
+	 * @param item 更新内容を含むリソースアイテム
+	 * @return 更新されたリソースアイテム
+	 */
+	@Override
+	protected ScheduleResourceItem doUpdate(ScheduleResourceItem item) {
+
+		Schedule updatingEntity = findSchedule(item.getScheduleId());
+
+		itemToEntity(updatingEntity, item);
+
+		repository.save(updatingEntity);
+
+		item.setCreateUserName(updatingEntity.getCreateUser().getName());
+		return item;
+	}
+
+	/**
+	 * リソースアイテムの内容をScheduleエンティティに設定します.<br>
+	 *
+	 * @param entity Scheduleオブジェクト(Entity)
+	 * @param item Scheduleリソースアイテムオブジェクト
+	 */
+	private void itemToEntity(Schedule entity, ScheduleResourceItem item) {
+
+		// userBeansの関連の更新
+		entity.setUserBeans(itemUsersToEntityUsers(entity.getUserBeans(), item.getUserIds()));
+
+		entity.setTitle(item.getTitle());
+		entity.setCategory(item.getCategory());
+
+		// DateBeansの関連の更新
+		entity.setDateBeans(itemDatesToEntityDates(entity, item.getDates()));
+
+		entity.setStartTime(item.getStartTime());
+		entity.setFinishTime(item.getFinishTime());
+		entity.setDetail(item.getDetail());
+		entity.setPlace(item.getPlace());
+	}
+
+	/**
+	 * リソースアイテムのユーザーリストの内容をエンティティのユーザーリストに反映します.<br>
+	 * エンティティのユーザーリストに既に含むものはそのままとし、足りないユーザーのPersonエンティティを取得して加えます.
+	 *
+	 * @param entityUserBeans エンティティのユーザーリスト(Personエンティティのリスト)
+	 * @param itemUserIds リソースアイテムのユーザーリスト(IDのリスト)
+	 * @return 反映後のユーザーリスト(Personエンティティのリスト)
+	 */
+	private List<Person> itemUsersToEntityUsers(List<Person> entityUserBeans, List<String> itemUserIds) {
+
+		List<Person> resultUserList = new ArrayList<>();
+
+		// 足りないユーザーを抽出するためリストのコピーを作成
+		List<String> userListInItem = new ArrayList<>(itemUserIds);
+
+		// 既存のエンティティリストに含まれるものはそのまま新しいリストに含め、コピーから削除
+		for (Person existingPerson : entityUserBeans) {
+			if (itemUserIds.contains(existingPerson.getPersonId())) {
+				resultUserList.add(existingPerson);
+				userListInItem.remove(existingPerson.getPersonId());
+			}
+		}
+
+		// コピーに残ったIDは既存のエンティティリストに含まないため、リポジトリから取得して新しいリストに含める
+		for (String newPersonId : userListInItem) {
+			resultUserList.add(findRelatedPerson(newPersonId));
+		}
+
+		return resultUserList;
+	}
+
+	/**
+	 * リソースアイテムの予定日付リストの内容をエンティティの予定日付リストに反映します.<br>
+	 * エンティティのユーザーリストに既に含むものはそのままとし、足りない日付のScheduleDateエンティティを取得して加えます.
+	 *
+	 * @param entity Scheduleエンティティ
+	 * @param itemDates
+	 * @return
+	 */
+	private List<ScheduleDate> itemDatesToEntityDates(Schedule entity, List<String> itemDates) {
+
+		List<ScheduleDate> resultDatesList = new ArrayList<>();
+
+		Iterator<String> dateItr = itemDates.iterator();
+		for (ScheduleDate dateBean : entity.getDateBeans()) {
+
+			// 日付のマッチングは行わず、取得順に再設定する
+			if (dateItr.hasNext()) {
+				ScheduleDate oldDateBean = dateBean;
+				oldDateBean.setDate(removeDateSeparator(dateItr.next()));
+				resultDatesList.add(oldDateBean);
+			}
+		}
+
+		// 元より多い分は新規
+		while (dateItr.hasNext()) {
+			resultDatesList.add(new ScheduleDate(entity, removeDateSeparator(dateItr.next())));
+		}
+		return resultDatesList;
+	}
+
+	/**
+	 * 日付文字列のセパレータ(スラッシュ)を除去し、8桁数値に変換します.
+	 *
+	 * @param slashSeparatedDate セパレータ付き日付文字列
+	 * @return 日付数値
+	 */
+	private int removeDateSeparator(String slashSeparatedDate) {
+
+		DateFormat slashSeparation = new SimpleDateFormat(FORMAT_STR_SLASH_SEPARATION);
+		DateFormat int8 = new SimpleDateFormat(FORMAT_STR_INT8);
+
+		String dateStr;
+		try {
+			dateStr = int8.format(slashSeparation.parse(slashSeparatedDate));
+		} catch (ParseException e) {
+			throw new SyncException(e);
+		}
+
+		return Integer.parseInt(dateStr);
+	}
+
+	/**
+	 * 認証情報からログインユーザーIDを取得します.
+	 *
+	 * @return ログインユーザーID.
+	 */
+	private Person loginUser() {
+
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		String username;
+		if (principal instanceof UserDetails) {
+			username = ((UserDetails) principal).getUsername();
+		} else {
+			username = principal.toString();
+		}
+
+		return personRepository.findOne(username);
+	}
+
+	/**
+	 * deleteメソッドのリソース別独自処理. <br>
+	 * 論理削除のため、エンティティは変更せずにIDのみ設定された空のリソースアイテムを返します.
+	 *
+	 * @param targetItemId リソースアイテムのID
+	 * @return 削除されたアイテムを表すリソースアイテムオブジェクト
+	 */
+	@Override
+	protected ScheduleResourceItem doDelete(String targetItemId) {
+
+		findSchedule(targetItemId);
+		return new ScheduleResourceItem(targetItemId);
+	}
+
+	/**
+	 * このリソースのリソースアイテムのIDから1件のエンティティをリポジトリを検索して取得します. <br>
+	 *
+	 * @param scheduleId リソースアイテムのID
+	 * @return Scheduleエンティティ
+	 */
+	private Schedule findSchedule(String scheduleId) {
+
+		Schedule found = repository.findOne(scheduleId);
+		if (found == null) {
+			throw new BadRequestException("entity not found :" + scheduleId);
+		}
+		return found;
+	}
+
+	/**
+	 * このリソースのリソースアイテムと関連するPersonのIDから1件のエンティティをリポジトリを検索して取得します. <br>
+	 *
+	 * @param personId PersonのID
+	 * @return Personエンティティ
+	 */
+	private Person findRelatedPerson(String personId) {
+		Person found = personRepository.findOne(personId);
+		if (found == null) {
+			throw new BadRequestException("entity not found :" + personId);
+		}
+		return found;
+	}
+}
---
+++ hifiveSyncSample/src/main/java/com/htmlhifive/sync/sample/scd/ScheduleSpecifications.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright (C) 2012 NS Solutions Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.htmlhifive.sync.sample.scd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Fetch;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.JoinType;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.domain.Specifications;
+
+import com.htmlhifive.sync.common.ResourceItemCommonData;
+
+/**
+ * Scheduleをリソースクエリによって検索するためのSpring Specification定義クラス.
+ */
+public class ScheduleSpecifications {
+
+	/**
+	 * 「指定された共通データが対応するリソースアイテムであり、かつデータ項目が指定された条件に合致する」というクエリ仕様を表現します.
+	 *
+	 * @param commonDataList リソースアイテム共通データ
+	 * @param conditions クエリ条件
+	 * @return Specificationsオブジェクト
+	 */
+	public static Specifications<Schedule> parseConditions(List<ResourceItemCommonData> commonDataList,
+			Map<String, String[]> conditions) {
+
+		List<Specification<Schedule>> specList = new ArrayList<>();
+
+		List<String> targetItemIdList = new ArrayList<>();
+		for (ResourceItemCommonData common : commonDataList) {
+			targetItemIdList.add(common.getTargetItemId());
+		}
+		if (!targetItemIdList.isEmpty()) {
+			specList.add(isInIds(targetItemIdList.toArray(new String[] {})));
+		}
+
+		for (String cond : conditions.keySet()) {
+			switch (cond) {
+				case ("sceduleId"):
+					specList.add(isInIds(conditions.get(cond)));
+					break;
+				case ("personIds"):
+					specList.add(isInUserIds(conditions.get(cond)));
+					break;
+				case ("userOrCreator"):
+					specList.add(isInUserIdsAndCreateUserId(conditions.get(cond)));
+					break;
+				//				case ("date-from"):
+				//					specList.add(isGtOrEqSomeDates(conditions.get(cond)[0]));
+				//TODO:日付関係のSpec実装
+				// ・・・
+
+				default:
+					// 何もしない
+			}
+		}
+
+		if (specList.isEmpty()) {
+			return null;
+		}
+
+		Specifications<Schedule> specs = Specifications.where(specList.get(0));
+		for (Specification<Schedule> spec : specList.subList(1, specList.size())) {
+
+			specs = specs.and(spec);
+		}
+
+		return specs;
+	}
+
+	/**
+	 * 配列に含むIDのいずれかに合致するScheduleを抽出するSpecificationを返します.
+	 *
+	 * @param ids IDの配列
+	 * @return Specificationオブジェクト
+	 */
+	public static Specification<Schedule> isInIds(final String... ids) {
+		return new Specification<Schedule>() {
+
+			@Override
+			public Predicate toPredicate(Root<Schedule> root, CriteriaQuery<?> cq, CriteriaBuilder builder) {
+
+				return root.get(root.getModel().getSingularAttribute("scheduleId", String.class)).in((Object[]) ids);
+			}
+		};
+	}
+
+	/**
+	 * 配列に含むpersonIdのいずれかがuserBeansのIDのいずれかに合致するScheduleを抽出するSpecificationを返します.
+	 *
+	 * @param personIds personIdの配列
+	 * @return Specificationオブジェクト
+	 */
+	public static Specification<Schedule> isInUserIds(final String[] personIds) {
+		return new Specification<Schedule>() {
+
+			@Override
+			public Predicate toPredicate(Root<Schedule> root, CriteriaQuery<?> cq, CriteriaBuilder builder) {
+
+				return ((Join<?, ?>) root.fetch("userBeans")).get("personId").in((Object[]) personIds);
+			}
+		};
+	}
+
+	/**
+	 * 配列に含むpersonIdのいずれかが、userBeansのIDのいずれか、またはcreateUserIdに合致するScheduleを抽出するSpecificationを返します.
+	 *
+	 * @param personIds personIdの配列
+	 * @return Specificationオブジェクト
+	 */
+	public static Specification<Schedule> isInUserIdsAndCreateUserId(final String[] personIds) {
+		return new Specification<Schedule>() {
+
+			@Override
+			public Predicate toPredicate(Root<Schedule> root, CriteriaQuery<?> cq, CriteriaBuilder builder) {
+
+				Predicate inUserIds = ((Join<?, ?>) root.fetch("userBeans", JoinType.LEFT)).get("personId").in(
+						(Object[]) personIds);
+				Predicate inCreator = ((Join<?, ?>) root.fetch("createUser")).get("personId").in((Object[]) personIds);
+				return builder.or(inUserIds, inCreator);
+			}
+		};
+	}
+
+	/**
+	 * 指定された日付以降の予定日を持つScheduleを抽出するSpecificationを返します.
+	 *
+	 * @param dateStr 日付文字列(スラッシュ区切り、「日」省略可)
+	 * @return Specificationオブジェクト
+	 */
+	public static Specification<Schedule> isGtOrEqSomeDates(final String dateStr) {
+		return new Specification<Schedule>() {
+			@Override
+			public Predicate toPredicate(Root<Schedule> root, CriteriaQuery<?> cq, CriteriaBuilder builder) {
+
+				Fetch<Schedule, String> fetch = root.fetch(root.getModel().getCollection("dates", String.class),
+						JoinType.LEFT);
+
+				System.out.println(fetch);
+				return null;
+			}
+		};
+
+	}
+	//
+	//	private static Date parseWithFirstDate(String dateStr) {
+	//
+	//		String[] dateArray = dateStr.split("/");
+	//
+	//		if (dateArray.length < 1) {
+	//			return null;
+	//		}
+	//
+	//		Calendar cal = Calendar.getInstance();
+	//		cal.clear();
+	//
+	//		cal.set(Calendar.YEAR, Integer.parseInt(dateArray[0]));
+	//		cal.set(Calendar.MONTH,
+	//				dateArray.length >= 2 ? Integer.parseInt(dateArray[1]) - 1 : cal.getActualMinimum(Calendar.MONTH));
+	//		cal.set(Calendar.DATE,
+	//				dateArray.length >= 3 ? Integer.parseInt(dateArray[2]) : cal.getActualMinimum(Calendar.DATE));
+	//
+	//		return cal.getTime();
+	//	}
+	//
+	//	private static Date parseWithEndDate(String dateStr) {
+	//
+	//		String[] dateArray = dateStr.split("/");
+	//
+	//		if (dateArray.length < 1) {
+	//			return null;
+	//		}
+	//
+	//		Calendar cal = Calendar.getInstance();
+	//		cal.clear();
+	//
+	//		cal.set(Calendar.YEAR, Integer.parseInt(dateArray[0]));
+	//		cal.set(Calendar.MONTH,
+	//				dateArray.length >= 2 ? Integer.parseInt(dateArray[1]) - 1 : cal.getActualMaximum(Calendar.MONTH));
+	//		cal.set(Calendar.DATE,
+	//				dateArray.length >= 3 ? Integer.parseInt(dateArray[2]) : cal.getActualMaximum(Calendar.DATE));
+	//
+	//		return cal.getTime();
+	//	}
+}
