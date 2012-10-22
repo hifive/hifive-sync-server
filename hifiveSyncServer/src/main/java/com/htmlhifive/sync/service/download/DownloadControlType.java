3115d420dd60a65a4151aee96fe4ace58115ab9d
kishigam <kishigami.kazuhiro@ns-sol.co.jp>
2012-10-22 21:16:17 +0900

    コメント追加、仕様変更反映(下り更新制御のVERIFYを削除).

--- hifiveSyncServer/src/main/java/com/htmlhifive/sync/service/download/DownloadControlType.java
+++ hifiveSyncServer/src/main/java/com/htmlhifive/sync/service/download/DownloadControlType.java
@@ -1,19 +1,21 @@
 package com.htmlhifive.sync.service.download;
 
+import com.htmlhifive.sync.service.Synchronizer;
+
+/**
+ * {@link Synchronizer}における下り更新の同期制御方式を表す列挙型.
+ *
+ * @author kishigam
+ */
 public enum DownloadControlType {
 
 	/**
-	 *
+	 * 下り更新対象のリソースアイテムに対して読み取りロックを行ってから取得します.<br>
 	 */
 	READ_LOCK,
 
 	/**
-	 *
-	 */
-	VERIFY,
-
-	/**
-	 *
+	 * 下り更新において同期制御を行いません.<br>
 	 */
 	NONE,
 }
--- hifiveSyncServer/src/main/java/com/htmlhifive/sync/service/upload/UploadControlType.java
+++ hifiveSyncServer/src/main/java/com/htmlhifive/sync/service/upload/UploadControlType.java
@@ -1,19 +1,29 @@
 package com.htmlhifive.sync.service.upload;
 
+import com.htmlhifive.sync.service.Synchronizer;
+
+/**
+ * {@link Synchronizer}における上り更新の同期制御方式を表す列挙型.
+ *
+ * @author kishigam
+ */
 public enum UploadControlType {
 
 	/**
-	 *
+	 * 上り更新対象のリソースアイテムに対して「予約」を行い、更新を実行します.<br>
+	 * 具体的には、"for update"操作によるロックを実行します.
 	 */
 	RESERVE,
 
 	/**
-	 *
+	 * 上り更新対象のリソースアイテムの更新実行順をソートにより変更します.<br>
+	 * 更新順が一定になるためデッドロックが回避できます.<br>
+	 * 更新実行順を変更可能なアプリケーションのみ使用できます.
 	 */
 	AVOID_DEADLOCK,
 
 	/**
-	 *
+	 * 上り更新において同期制御を行いません.
 	 */
 	NONE,
 }
