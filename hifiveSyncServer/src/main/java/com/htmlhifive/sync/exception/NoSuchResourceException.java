13d97cab30cc0165aa949c6dc1950e5a27a0b110
kishigam <kishigami.kazuhiro@ns-sol.co.jp>
2013-01-15 13:05:06 +0900

    #2 リソースが見つからない場合にスローされる例外の新規追加.

---
+++ hifiveSyncServer/src/main/java/com/htmlhifive/sync/exception/NoSuchResourceException.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2012-2013 NS Solutions Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.htmlhifive.sync.exception;
+
+/**
+ * 指定されたリソースがアプリケーションで管理されているリソースに含まれていない場合にスローされる例外.
+ *
+ * @author kishigam
+ */
+public class NoSuchResourceException extends RuntimeException {
+
+	private static final long serialVersionUID = -4473178090921030434L;
+
+	/**
+	 * @see RuntimeException
+	 */
+	public NoSuchResourceException() {
+		super();
+	}
+
+	/**
+	 * @see RuntimeException
+	 */
+	public NoSuchResourceException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	/**
+	 * @see RuntimeException
+	 */
+	public NoSuchResourceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * @see RuntimeException
+	 */
+	public NoSuchResourceException(String message) {
+		super(message);
+	}
+
+	/**
+	 * @see RuntimeException
+	 */
+	public NoSuchResourceException(Throwable cause) {
+		super(cause);
+	}
+}
